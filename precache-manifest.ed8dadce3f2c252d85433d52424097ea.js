76f3ee4fa58b216077db87f82ce37a79d6c335e5
Kebtarum <mura_aktau@mail.ru>
2020-10-19 02:24:30 +0300

    Updates

---
+++ precache-manifest.ed8dadce3f2c252d85433d52424097ea.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbe6756e6f525c31ec88df3092eb79cf",
+    "url": "/tz-from-figma/index.html"
+  },
+  {
+    "revision": "85502a4144f0817343bd",
+    "url": "/tz-from-figma/static/css/main.b02d6403.chunk.css"
+  },
+  {
+    "revision": "2a827ba1912345394729",
+    "url": "/tz-from-figma/static/js/2.18095707.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/tz-from-figma/static/js/2.18095707.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85502a4144f0817343bd",
+    "url": "/tz-from-figma/static/js/main.77465888.chunk.js"
+  },
+  {
+    "revision": "64a18e2f125d51c653e4",
+    "url": "/tz-from-figma/static/js/runtime-main.9546bbf9.js"
+  },
+  {
+    "revision": "d6a89307b641c07efa5fceac4b9ef365",
+    "url": "/tz-from-figma/static/media/AddTask.d6a89307.svg"
+  },
+  {
+    "revision": "78f9a1bb181a2d1afb347ba6070fb8cc",
+    "url": "/tz-from-figma/static/media/Arrow.78f9a1bb.svg"
+  },
+  {
+    "revision": "6b897fa86c3413f97460c6c3e3c1ffe9",
+    "url": "/tz-from-figma/static/media/Back.6b897fa8.svg"
+  },
+  {
+    "revision": "fb1b193e7b22ee12eec1c8e465682474",
+    "url": "/tz-from-figma/static/media/Burger.fb1b193e.svg"
+  },
+  {
+    "revision": "6445bcaf75772eeb0e723169a2edcce7",
+    "url": "/tz-from-figma/static/media/Calendar.6445bcaf.svg"
+  },
+  {
+    "revision": "5e8a7496d21ea7c34e4465d03ec2b9a4",
+    "url": "/tz-from-figma/static/media/CloseBlack.5e8a7496.svg"
+  },
+  {
+    "revision": "5e50c2949e621c21ebc5e807c85a5774",
+    "url": "/tz-from-figma/static/media/CloseIcon.5e50c294.svg"
+  },
+  {
+    "revision": "bf95915a73adc79b4033ffa527e60877",
+    "url": "/tz-from-figma/static/media/Edit.bf95915a.svg"
+  },
+  {
+    "revision": "111e7f5f33bc479e7e9c0fbb4e8b0cff",
+    "url": "/tz-from-figma/static/media/Ellipse.111e7f5f.svg"
+  },
+  {
+    "revision": "1d219da3a2ec815aa51b81a0e68a9843",
+    "url": "/tz-from-figma/static/media/Exclude.1d219da3.svg"
+  },
+  {
+    "revision": "7e2da888f22ce701f32e4106d557edd1",
+    "url": "/tz-from-figma/static/media/GreenRect.7e2da888.svg"
+  },
+  {
+    "revision": "f4e55fe859e5a6f2d6092140c2b84d61",
+    "url": "/tz-from-figma/static/media/Oval.f4e55fe8.svg"
+  },
+  {
+    "revision": "8394be497f0e6d0b80863ffbbb66fbbc",
+    "url": "/tz-from-figma/static/media/RedRect.8394be49.svg"
+  },
+  {
+    "revision": "1e0534b1a87d0e7375dd1a3e737e6db7",
+    "url": "/tz-from-figma/static/media/Trash.1e0534b1.svg"
+  },
+  {
+    "revision": "07bbfc020d6d1bebb1104808efc37e62",
+    "url": "/tz-from-figma/static/media/YellowRect.07bbfc02.svg"
+  }
+]);
